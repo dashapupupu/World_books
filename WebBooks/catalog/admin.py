0a6a80ca368f9917c0352fc8b8e13ef07aaf1428
dashapupupu <dididi2037@mail.ru>
2024-09-26 21:49:39 +0300

    ЛЗ11 и ПЗ8
    В ЛЗ11 создали прототип сайта при помощи Django, создали модели данных для веб-приложения, использовали
    административную панель Django для управления сайтом. В ПЗ8 для сайта создали несколько таблиц на основе миграции. Поработали с данными в джанго админ добавив сведения о жанрах, авторах, книгах и об их состояниях.

---
+++ WebBooks/catalog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from.models import Author, Book, Genre, Language, Status, BookInstance
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Status)
+admin.site.register(BookInstance)
+
+# Register your models here.
