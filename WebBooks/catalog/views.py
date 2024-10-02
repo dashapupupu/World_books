cd666818abf241c74b29e045ac0bade9edc233a4
Babkina Sofia <babckina.sofi@yandex.ru>
2024-10-02 20:06:14 +0300

    ПЗ10 Формирование страниц.
    В ходу практической приобрели общее понятие о работе с полями форм.

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import *
+from .forms import AuthorsForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 def index(request):
   # Генерация "количеств" некоторых главных объектов
@@ -40,3 +45,60 @@ class BookListView(generic.ListView):
 class AuthorListView(generic.ListView):
  model = Author
  paginate_by = 4
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+  """Универсальный класс представления списка книг, находящихся в заказе у текущего пользователя."""
+  model = BookInstance
+  template_name = 'catalog/book_instance_list_borrowed_user.html'
+  paginate_by = 10
+def get_queryset(self):
+ BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='2').order_by('due_back')
+ return
+    
+
+def authors_add(request):
+  author = Author.objects.all()
+  authorsform = AuthorsForm()
+  return render(request, "catalog/authors_add.html",
+ {"form": authorsform, "author": author})
+
+def create(request):
+ if request.method == "POST":
+  author = Author()
+  author.first_name = request.POST.get("first_name")
+  author.last_name = request.POST.get("last_name")
+  author.save()
+  return HttpResponseRedirect("/authors_add/")
+
+def delete(request, id):
+  try:
+    author = Author.objects.get(id=id)
+    author.delete()
+    return HttpResponseRedirect("/authors_add/")
+  except Author.DoesNotExist:
+    return HttpResponseNotFound("<h2>Автор не найден</h2>")
+  
+def edit1(request, id):
+  author = Author.objects.get(id=id)
+  if request.method == "POST":
+    author.first_name = request.POST.get("first_name")
+    author.last_name = request.POST.get("last_name")  
+    author.date_of_birth = request.POST.get("date_of_birth")
+    author.date_of_death = request.POST.get("date_of_death")
+    author.save()
+    return HttpResponseRedirect("/authors_add/")
+  else:
+    return render(request, "edit1.html", {"author": author})
+  
+class BookCreate(CreateView):
+ model = Book
+ fields = '__all__'
+ success_url = reverse_lazy('books')
+class BookUpdate(UpdateView):
+ model = Book
+ fields = '__all__'
+ success_url = reverse_lazy('books')
+class BookDelete(DeleteView):
+ model = Book
+ fields = '__all__'
+ success_url = reverse_lazy('books')
